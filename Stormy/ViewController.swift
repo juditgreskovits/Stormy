ac6b0c98c8e0e4bd432dbd01fed127260dab0ed5
Judit Greskovits <juditgreskovits@gmail.com>
2014-10-17 13:53:58 +0100

    added location services

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var iconView: UIImageView!
     @IBOutlet weak var locationLabel: UILabel!
@@ -21,14 +22,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var refreshButton: UIButton!
     @IBOutlet weak var refreshActivityIndicator: UIActivityIndicatorView!
     
-    private let apiKey = "6da82739c588a45ea214840c076f3aef";
+    private let apiKey = "6da82739c588a45ea214840c076f3aef"
+    private let baseURL = "https://api.forecast.io/forecast/"
+    
+    let locationManager = CLLocationManager()
+    
+    let authorizationErrorMessage = "Without accessing your location the app can't tell give you weather info."
+    let networkErrorMessage = "Unable to load data. Connectivity error!"
+    let locationErrorMessage = "Unable to access your location."
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        refreshActivityIndicator.hidden = true
-        getCurrentWeatherData()
+        refresh()
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,9 +43,59 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func getCurrentWeatherData() {
-        let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
-        let forecastURL = NSURL(string: "37.8267,-122.423", relativeToURL: baseURL)
+    @IBAction func refresh() {
+        refreshButton.hidden = true
+        refreshActivityIndicator.hidden = false
+        refreshActivityIndicator.startAnimating()
+        getCurrentLocation()
+    }
+    
+    func getCurrentLocation() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        let status = CLLocationManager.authorizationStatus()
+        if(status == CLAuthorizationStatus.NotDetermined) {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        let authorized = status == CLAuthorizationStatus.AuthorizedWhenInUse || status == CLAuthorizationStatus.Authorized
+        println(authorized)
+        if(authorized) {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        let location: CLLocation = locations[0] as CLLocation
+        let coordinate: CLLocationCoordinate2D = location.coordinate
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(manager.location, completionHandler: { (placemarks, error) -> Void in
+            if(error == nil) {
+                if (placemarks.count > 0) {
+                    let placemark = placemarks[0] as CLPlacemark
+                    self.locationLabel.text = "\(placemark.subLocality), \(placemark.locality)"
+                    self.getCurrentWeatherData(coordinate.latitude, longitude: coordinate.longitude)
+                } else {
+                    self.handleErrorWithMessage(self.locationErrorMessage)
+                }
+            } else {
+                self.handleErrorWithMessage(self.locationErrorMessage)
+            }
+        })
+    }
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        handleErrorWithMessage(locationErrorMessage)
+    }
+    
+    func getCurrentWeatherData(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        
+        let relativeToURL = NSURL(string: "\(baseURL)\(apiKey)/")
+        let forecastURL = NSURL(string: "\(latitude),\(longitude)?units=uk", relativeToURL: relativeToURL)
         // let weatherData = NSData.dataWithContentsOfURL(forecastURL, options: nil, error: nil)
         // println(weatherData)
         let sharedSession = NSURLSession.sharedSession()
@@ -54,26 +111,14 @@ class ViewController: UIViewController {
                 
                 dispatch_async(dispatch_get_main_queue(), { () -> Void in
                     
-                    self.iconView.image = currentWeather.icon!
-                    self.currentTimeLabel.text = "At \(currentWeather.currentTime!) it is"
-                    self.temperatureLabel.text = "\(currentWeather.temperature)"
-                    self.humidityLabel.text = "\(currentWeather.humidity)"
-                    self.precipitationLabel.text = "\(currentWeather.precipProbability)"
-                    self.summaryLabel.text = currentWeather.summary
+                    self.updateView(currentWeather)
                     
                     self.resetRefresh()
                     
                 })
             } else {
-                let networkIssueController = UIAlertController(title: "Error", message: "Unable to load data. Connectivity error!", preferredStyle: .Alert)
                 
-                let okButton = UIAlertAction(title: "OK", style: .Default, handler: nil)
-                networkIssueController.addAction(okButton)
-                
-                let cancelButton = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
-                networkIssueController.addAction(cancelButton)
-                
-                self.presentViewController(networkIssueController, animated: true, completion: nil)
+                self.handleErrorWithMessage(self.networkErrorMessage)
                 
                 self.resetRefresh()
             }
@@ -81,18 +126,31 @@ class ViewController: UIViewController {
         downloadTask.resume()
     }
     
+    func updateView(currentWeather: Current) {
+        iconView.image = currentWeather.icon!
+        currentTimeLabel.text = "At \(currentWeather.currentTime!) it is"
+        temperatureLabel.text = "\(currentWeather.temperature)"
+        humidityLabel.text = "\(currentWeather.humidity)"
+        precipitationLabel.text = "\(currentWeather.precipProbability)"
+        summaryLabel.text = currentWeather.summary
+    }
+    
     func resetRefresh() {
         refreshActivityIndicator.stopAnimating()
         refreshActivityIndicator.hidden = true
         refreshButton.hidden = false
     }
-
-    @IBAction func refresh() {
-        refreshButton.hidden = true
-        refreshActivityIndicator.hidden = false
-        refreshActivityIndicator.startAnimating()
-        getCurrentWeatherData()
+    
+    func handleErrorWithMessage(message: String) {
+        let networkIssueController = UIAlertController(title: "Error", message: message, preferredStyle: .Alert)
+        
+        let okButton = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        networkIssueController.addAction(okButton)
+        
+        let cancelButton = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        networkIssueController.addAction(cancelButton)
+        
+        presentViewController(networkIssueController, animated: true, completion: nil)
     }
-
 }
 
